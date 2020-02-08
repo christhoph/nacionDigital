769ac7ff3aeaa37368f23fee0270d3d174cecff8
christhoph <cristoph.solis@outlook.com>
2020-02-08 10:52:31 -0500

    Remove firebase reference and fix code
    * prettier files

--- js/app.js
+++ js/app.js
@@ -1,100 +1,65 @@
-
 $(() => {
+  var btnSize = $("#btn-size");
 
-    var btnSize = $('#btn-size')
+  if ($(window).innerWidth() > 576) {
+    $("#box2").removeClass("rotation");
+    $("#box1").addClass("rotation");
+  } else {
+    $("#box1").removeClass("rotation");
+    $("#box2").addClass("rotation");
+  }
 
-    if( $(window).innerWidth() > 576 ){
+  $(window).resize(() => {
+    var inputSize = $("#size").val();
 
-        $('#box2').removeClass('rotation')
-        $('#box1').addClass('rotation')
+    if ($(window).innerWidth() > 576) {
+      $("#box2").removeClass("rotation");
+      $("#box1").addClass("rotation");
 
-    } else{
+      if (inputSize != "") {
+        $("#box1").css({ height: `${inputSize}px`, width: `${inputSize}px` });
+        $("#box2").css({ height: `${inputSize}px`, width: `${inputSize}px` });
+      } else {
+        $("#box1").css({ height: `150px`, width: `150px` });
+        $("#box2").css({ height: `150px`, width: `150px` });
+      }
+    } else {
+      var boxSize = inputSize - inputSize * 0.3;
 
-        $('#box1').removeClass('rotation')
-        $('#box2').addClass('rotation')
+      $("#box1").removeClass("rotation");
+      $("#box2").addClass("rotation");
 
+      if (inputSize != "") {
+        $("#box1").css({ height: `${inputSize}px`, width: `${inputSize}px` });
+        $("#box2").css({ height: `${boxSize}px`, width: `${boxSize}px` });
+      } else {
+        $("#box1").css({ height: `150px`, width: `150px` });
+        $("#box2").css({ height: `105px`, width: `105px` });
+      }
     }
+  });
 
-    $(window).resize(() => {
+  btnSize.on("click", () => {
+    var inputSize = $("#size").val();
 
-        var inputSize = $('#size').val()
+    if ($(window).innerWidth() > 576) {
+      if (inputSize != "") {
+        $("#box1").css({ height: `${inputSize}px`, width: `${inputSize}px` });
+        $("#box2").css({ height: `${inputSize}px`, width: `${inputSize}px` });
+      } else {
+        $("#box1").css({ height: `150px`, width: `150px` });
+        $("#box2").css({ height: `150px`, width: `150px` });
+      }
+    } else {
+      var boxSize = inputSize - inputSize * 0.3;
 
-        if( $(window).innerWidth() > 576 ){
-
-            $('#box2').removeClass('rotation')
-            $('#box1').addClass('rotation')
-
-            if(inputSize != ''){
-
-                $('#box1').css({'height': `${inputSize}px`, 'width': `${inputSize}px`})
-                $('#box2').css({'height': `${inputSize}px`, 'width': `${inputSize}px`})
-
-            } else{
-
-                $('#box1').css({'height': `150px`, 'width': `150px`})
-                $('#box2').css({'height': `150px`, 'width': `150px`})
-
-            }
-
-        } else{
-
-            var boxSize = inputSize - (inputSize * 0.3)
-
-            $('#box1').removeClass('rotation')
-            $('#box2').addClass('rotation')
-
-            if(inputSize != ''){
-
-                $('#box1').css({'height': `${inputSize}px`, 'width': `${inputSize}px`})
-                $('#box2').css({'height': `${boxSize}px`, 'width': `${boxSize}px`})
-
-            } else{
-
-                $('#box1').css({'height': `150px`, 'width': `150px`})
-                $('#box2').css({'height': `105px`, 'width': `105px`})
-
-            }
-
-        }
-
-    })
-
-    btnSize.on('click', () => {
-
-        var inputSize = $('#size').val()
-
-        if( $(window).innerWidth() > 576 ){
-
-            if(inputSize != ''){
-
-                $('#box1').css({'height': `${inputSize}px`, 'width': `${inputSize}px`})
-                $('#box2').css({'height': `${inputSize}px`, 'width': `${inputSize}px`})
-
-            } else{
-
-                $('#box1').css({'height': `150px`, 'width': `150px`})
-                $('#box2').css({'height': `150px`, 'width': `150px`})
-
-            }
-
-        } else{
-
-            var boxSize = inputSize - (inputSize * 0.3)
-
-            if(inputSize != ''){
-
-                $('#box1').css({'height': `${inputSize}px`, 'width': `${inputSize}px`})
-                $('#box2').css({'height': `${boxSize}px`, 'width': `${boxSize}px`})
-
-            } else{
-
-                $('#box1').css({'height': `150px`, 'width': `150px`})
-                $('#box2').css({'height': `105px`, 'width': `105px`})
-                
-            }
-
-        }
-
-    })
-
-})
+      if (inputSize != "") {
+        $("#box1").css({ height: `${inputSize}px`, width: `${inputSize}px` });
+        $("#box2").css({ height: `${boxSize}px`, width: `${boxSize}px` });
+      } else {
+        $("#box1").css({ height: `150px`, width: `150px` });
+        $("#box2").css({ height: `105px`, width: `105px` });
+      }
+    }
+  });
+});
